f4fa25f669a6ad5a647bc5d323a1be9b5cdecbc3
caltek10 <Vas9_Lagun_1994@mail.ru>
2020-05-26 00:31:38 +0300

    git add

--- my_portal/pages/models.py
+++ my_portal/pages/models.py
@@ -5,6 +5,7 @@ class Pages(models.Model):
     title = models.CharField(max_length=150)
     text = models.TextField()
     created = models.DateTimeField(auto_created=True, null=True, blank=True)
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return "%s - %s" % (self.pk, self.title)

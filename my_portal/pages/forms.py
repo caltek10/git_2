d417769b19a981385fc54905e7f970d911f49187
caltek10 <Vas9_Lagun_1994@mail.ru>
2020-05-28 01:37:44 +0300

    git add

--- my_portal/pages/forms.py
+++ my_portal/pages/forms.py
@@ -8,3 +8,10 @@ from django import forms
 class CommentForm(forms.Form):
     text = forms.CharField(label='Текст комментария', max_length=250)
     page = forms.IntegerField(label='id страницы')
+
+    def clean_page(self):
+        page_id = self.cleaned_data['page']
+        if not Pages.objects.filter(pk=self.cleaned_data['page']).exist():
+            raise forms.ValidationError("Page with id = %s doesn't exist." % self.cleaned_data['page'])
+
+        return page_id
